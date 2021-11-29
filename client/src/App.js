9bbcb14a09be0f688ab3eedc1f3bd1f2378a4b29
saraiyakrs <718618@sylvaniastudents.org>
2021-11-29 14:22:05 -0500

    added nv + sylvania school images + added basic table

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,69 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>
         Northview Homework Matrix v2.0
+      </h1>
+      <header className="App-header">
+        <img src="https://www.sylvania.k12.oh.us/GalleryImages/NVsealwhitebkgrnd21.png" alt="NorthviewLogo"></img>
+        <img src="https://www.sylvaniaschools.org/sysimages/LogoHeader3.png" alt="Sylvania Schools"></img>
       </header>
+
+
+      <body>
+        <table>
+          <tr>
+            <th>Week </th>
+            <th>Day</th>
+            <th>Department</th>
+            <th>Teacher</th>
+            <th>Subject</th>
+            <th>Assignment</th>
+          </tr>
+          <tr>
+            <td>11/29 - 12/3 </td>
+            <td>11/29</td>
+            <td>Math</td>
+            <td></td>
+            <td></td>
+            <td>Read + pg...</td>
+          </tr>
+          <tr>
+            <td>11/29 - 12/3 </td>
+            <td>11/30</td>
+            <td>Math</td>
+            <td></td>
+            <td></td>
+            <td>Read + pg...</td>
+          </tr>
+          <tr>
+            <td>11/29 - 12/3 </td>
+            <td>12/1</td>
+            <td>Math</td>
+            <td></td>
+            <td></td>
+            <td>Read + pg...</td>
+          </tr>
+          <tr>
+            <td>11/29 - 12/3 </td>
+            <td>12/2</td>
+            <td>Math</td>
+            <td></td>
+            <td></td>
+            <td>Read + pg...</td>
+          </tr>
+          <tr>
+            <td>11/29 - 12/3 </td>
+            <td>12/3</td>
+            <td>Math</td>
+            <td></td>
+            <td></td>
+            <td>Read + pg...</td>
+          </tr>
+
+        </table>
+      </body>
+
     </div>
   );
 }
