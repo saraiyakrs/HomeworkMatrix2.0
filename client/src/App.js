389ee6111d7fe381334a7ff06b89fca8673359d6
saraiyakrs <718618@sylvaniastudents.org>
2021-11-29 13:23:52 -0500

    started client side with change

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Northview Homework Matrix ... but better!
+        Northview Homework Matrix v2.0
       </header>
     </div>
   );
