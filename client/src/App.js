456bfc8cc5f2e1fe757c2441bfcb3047a08ac8ed
saraiyakrs <718618@sylvaniastudents.org>
2021-12-01 12:59:29 -0500

    added tabs and switching feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,19 @@
+import './index.css';
 import './App.css';
-
+import React from 'react';
+import Tabs from "./Components/TabComponent/Tabs";
 function App() {
   return (
     <div className="App">
-      <h1>
-        Northview Homework Matrix v2.0
-      </h1>
-      <header className="App-header">
-        <img src="https://www.sylvania.k12.oh.us/GalleryImages/NVsealwhitebkgrnd21.png" alt="NorthviewLogo"></img>
-        <img src="https://www.sylvaniaschools.org/sysimages/LogoHeader3.png" alt="Sylvania Schools"></img>
-      </header>
-
-
-      <body>
-        <table>
-          <tr>
-            <th>Week </th>
-            <th>Day</th>
-            <th>Department</th>
-            <th>Teacher</th>
-            <th>Subject</th>
-            <th>Assignment</th>
-          </tr>
-          <tr>
-            <td>11/29 - 12/3 </td>
-            <td>11/29</td>
-            <td>Math</td>
-            <td></td>
-            <td></td>
-            <td>Read + pg...</td>
-          </tr>
-          <tr>
-            <td>11/29 - 12/3 </td>
-            <td>11/30</td>
-            <td>Math</td>
-            <td></td>
-            <td></td>
-            <td>Read + pg...</td>
-          </tr>
-          <tr>
-            <td>11/29 - 12/3 </td>
-            <td>12/1</td>
-            <td>Math</td>
-            <td></td>
-            <td></td>
-            <td>Read + pg...</td>
-          </tr>
-          <tr>
-            <td>11/29 - 12/3 </td>
-            <td>12/2</td>
-            <td>Math</td>
-            <td></td>
-            <td></td>
-            <td>Read + pg...</td>
-          </tr>
-          <tr>
-            <td>11/29 - 12/3 </td>
-            <td>12/3</td>
-            <td>Math</td>
-            <td></td>
-            <td></td>
-            <td>Read + pg...</td>
-          </tr>
-
-        </table>
-      </body>
-
+        <header>
+          <h1>
+              Northview Homework Matrix Login Page
+              
+          </h1>
+        </header>
+      <Tabs />
     </div>
+    
   );
 }
-
 export default App;
