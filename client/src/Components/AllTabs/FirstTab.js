0b65d639e3523eb49685ea98529b525a293d486d
saraiyakrs <718618@sylvaniastudents.org>
2021-12-01 14:20:14 -0500

    added links for students

--- client/src/Components/AllTabs/FirstTab.js
+++ client/src/Components/AllTabs/FirstTab.js
@@ -2,8 +2,16 @@ import React from "react";
 const FirstTab = () => {
   return (
     <div className="FirstTab">
-      <p>HELLO THERE</p>
-      
+      <p>FOR STUDENTS ONLY</p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <li><a href="https://sylvania.schoology.com/home" target="_blank">SCHOOLOGY</a></li>
+      <li><a href="https://docs.google.com/spreadsheets/d/1l60K99adjO2rzIJFyoQwgsqnL9hDHJtBD5l9Mp3Qpe8/edit#gid=1583002944" target="_blank">HW Matrix</a></li>
+
+      <p>LOGIN IN BEFORE ACESSING COURSES AND ASSIGNMENTS</p>
+      <li><a href="#login">LOGIN</a></li>
+
       {/* First tab content will go here */}
      
       
